0da10595139c58d9d7d43ed92b2f8e020b919a94
Dũng <114909128+dangtiendung1409@users.noreply.github.com>
2024-01-20 18:36:34 +0700

    Add files via upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProjectGroup3.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult order()
+        {
+            return View("OrderManagement/order");
+        }
+        public IActionResult customer() 
+        {
+            return View("CustomerManagement/customer");
+        }
+        public IActionResult product()
+        {
+            return View("ProductManagement/product");
+        }
+
+        public IActionResult addProduct()
+        {
+            return View("ProductManagement/addProduct");
+        }
+        public IActionResult editProduct()
+        {
+            return View("ProductManagement/editProduct");
+        }
+    }
+}
+
---
+++ Controllers/PageController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProSem3.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Favourite()
+        {
+            return View();
+        }
+        public IActionResult Category()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Thankyou()
+        {
+            return View();
+        }
+    }
+}
